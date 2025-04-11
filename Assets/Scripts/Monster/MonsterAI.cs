bf5099a63abf70d01f6085173de80a5460b1677a
shane5828 <72241385+shane5828@users.noreply.github.com>
2025-04-12 03:09:56 +0900

    25.04.12 레전드 ㅋㅋ

--- Assets/Scripts/Monster/MonsterAI.cs
+++ Assets/Scripts/Monster/MonsterAI.cs
@@ -71,6 +71,7 @@ public class MonsterAI : MonoBehaviourPunCallbacks, IPunObservable
                 if (monsterInfo.attackTimer <= 0)
                 {
                     Attack();
+                    canMove = false;
                 }
                 else
                 {
@@ -148,7 +149,6 @@ public class MonsterAI : MonoBehaviourPunCallbacks, IPunObservable
 
     public virtual void Attack() // todo -> attacking animation
     {
-        canMove = false;
         if (animator != null)
             animator.SetTrigger("StartAttack");
         AttackBoundary.SetActive(true);
