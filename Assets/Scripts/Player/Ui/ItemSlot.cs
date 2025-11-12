f2b593e37d18b765707a49fe94623acadbe19b2c
ts <rlaxotjd0403@gmail.com>
2025-11-12 16:18:43 +0900

    25.11.12 작업중
    아이템 칸 작업중

---
+++ Assets/Scripts/Player/Ui/ItemSlot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    GameObject Icon()
+    {
+        if (transform.childCount > 0)
+            return transform.GetChild(0).gameObject;
+        else 
+            return null;
+    }
+
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (Icon() == null)
+        {
+            IconDrag.beingDraggedIcon.transform.SetParent(transform);
+            IconDrag.beingDraggedIcon.transform.position = transform.position;
+        }
+    }
+}
